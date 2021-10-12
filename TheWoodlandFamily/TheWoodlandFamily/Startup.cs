554aa6bc66091b20d385160560a72787c31d3dc9
MaraNatsu <ritamaniuk@ukr.net>
2021-10-13 00:04:51 +0300

    Add hub

--- TheWoodlandFamily/TheWoodlandFamily/Startup.cs
+++ TheWoodlandFamily/TheWoodlandFamily/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using EFDataAccessLibrary.DataAccess;
 using Microsoft.AspNetCore.Http;
+using TheWoodlandFamily.Hubs;
 
 namespace TheWoodlandFamily
 {
@@ -28,6 +29,8 @@ namespace TheWoodlandFamily
             {
                 options.UseSqlServer(Configuration.GetConnectionString("Default"));
             });
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +50,7 @@ namespace TheWoodlandFamily
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
